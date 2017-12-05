8bbecdcaff6c34e0064d8d67a2aee40f01112f0f
Alex Zwir <alexzwir@gmail.com>
2017-12-04 22:51:43 -0200

    Adding the heroku app

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEGUB',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','https://zwodonto.herokuapp.com/','http://zwodonto.herokuapp.com/']
+ALLOWED_HOSTS = ['.localhost','127.0.0.1','herokuapp.com']
 
 # Application definition
 
