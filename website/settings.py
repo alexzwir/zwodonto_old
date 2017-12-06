fabfdc2c821ba6321ff5de552acbf1fc97ff96d6
Alex Zwir <alexzwir@gmail.com>
2017-12-05 22:41:21 -0200

    Adding the zwodonto.com.br as an allowed host

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEGUB',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['.localhost','127.0.0.1','herokuapp.com']
+ALLOWED_HOSTS = ['.localhost','127.0.0.1','herokuapp.com','zwodonto.com.br']
 
 # Application definition
 
